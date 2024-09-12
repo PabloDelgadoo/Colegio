d7bb46f29906adedd1b974c2daaa1a9ec2628b3f
pc0409 <pc0409@PC0409>
2024-09-12 18:29:43 -0300

    Agregar archivos de proyecto.

---
+++ AccesodeDatos/DBEstudiantes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AccesodeDatos
+{
+    public class DBEstudiantes
+    {
+        private string conexion = "MiCadena";
+        public void AgregarEstudiantes(string nombre, string apellido, DateTime fechanacimiento, string nivelstudio)
+        {
+            using (SqlConnection conectar = new SqlConnection(conexion))
+            {
+                conectar.Open();
+                string query = "INSERT INTO ESTUDIANTES(Nombre, Apellido, FechaNacimiento,NivelEstudio)"+"VALUES(@Nombre, @Apellido, @FechaNacimiento,@NivelEstudio)";
+                SqlCommand cmd = new SqlCommand(query, conectar);
+                cmd.Parameters.AddWithValue("@Nombre", nombre);
+                cmd.Parameters.AddWithValue("@Apellido", apellido);
+                cmd.Parameters.AddWithValue("@FechaNacimiento", fechanacimiento);
+                cmd.Parameters.AddWithValue("@NivelEstudio", nivelstudio);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public SqlDataReader ObtenerEstudiantes()
+        {
+            SqlConnection conectar = new SqlConnection(conexion);
+            conectar.Open();
+            string query = "SELECT * FROM ESTUDIANTES";
+            SqlCommand cmd = new SqlCommand(query, conectar);
+            return cmd.ExecuteReader();
+        }
+    }
+}
---
+++ LogicadeNegocios/LogicaNegocio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicadeNegocios
+{
+    internal class LogicaNegocio
+    {
+        private DBEstudiantes estudiantesDB = new DBEstudiantes();
+        public void AgregarEstudiantes(string nombre, string apellido, DateTime fechaNacimiento, string nivelEstudio)
+        {
+              if(!string.IsNullOrEmpty(nombre) && !string.IsNullOrEmpty(apellido) && fechaNacimiento < DateTime.Now)
+            {
+                estudiantesDB.AgregarEstudiantes(nombre, apellido, fechaNacimiento, nivelEstudio);
+            }
+              else
+            {
+                throw new Exception("Los datos estan papus");
+            }
+        }
+        public SqlDataAdapter ObtenerEstudiantes()
+        {
+
+        }
+    }
+}
